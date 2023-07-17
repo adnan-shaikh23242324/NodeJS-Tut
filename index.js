bad8c24d4a76b3a2da1582cc408ec26e09a6cd4e
adnan-shaikh23242324 <107952487+adnan-shaikh23242324@users.noreply.github.com>
2023-07-17 16:55:11 +0530

    learn express js basics and ejs

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-import http from "http";
-import { generateLovePercent } from "./features.js";
-// import fs from "fs";
-import path from 'path';
+// import http from "http";
+// import { generateLovePercent } from "./features.js";
+// // import fs from "fs";
+// // import path from 'path';
 
-console.log(path.dirname("/home/random/index.html"));
+// // console.log(path.dirname("/home/random/index.html"));
 
-// const home = fs.readFileSync("./index.html");
-// console.log(home);
-// console.log(generateLovePercent());
-const server = http.createServer((req,res) => {
-    if(req.url === "/about"){
-         res.end(`<h1>LOve is ${generateLovePercent()}</h1>`)
-        }else if(req.url === "/"){
-            res.end(home)
-           }else if(req.url === "/contact"){
-            res.end("<h1>Contact Page</h1>")
-           }
-           else{
-            res.end("<h1>Page Not Found</h1>")
-           }
-});
+// // const home = fs.readFileSync("./index.html");
+// // console.log(home);
+// // console.log(generateLovePercent());
 
-server.listen(5000,()=>{
+// const server = http.createServer((req,res) => {
+//     console.log(req.method);
+//     if(req.url === "/about"){
+//          res.end(`<h1>LOve is ${generateLovePercent()}</h1>`)
+//         }else if(req.url === "/"){
+//             res.end("home");
+//            }else if(req.url === "/contact"){
+//             res.end("<h1>Contact Page</h1>")
+//            }
+//            else{
+//             res.end("<h1>Page Not Found</h1>")
+//            }
+// });
+
+// server.listen(5000,()=>{
+//     console.log("Server is Working");
+// })
+
+import { log } from "console";
+import express from "express";
+import path from "path";
+
+
+const app = express();
+
+app.use(express.static(path.join(path.resolve(),"public")));
+
+//setting up view engine
+app.set("view engine","index")
+
+app.get("/",(req,res) => {
+
+    // res.sendFile("index.html")
+
+    res.render("index.ejs",{name :"Adnan"});
+// res.json({
+//     success: true,
+//     products:[
+
+//     ]
+
+// });
+// res.status(400).send("Meri Marzi");
+// const pathlocation=path.resolve();
+// res.sendFile(path.join(pathlocation,"./index.html"))
+// res.render("index.ejs");
+})
+
+app.listen(5000,()=>{
     console.log("Server is Working");
 })
